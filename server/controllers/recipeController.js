8ccaac4c13d7041b81bb4fab409b004995e21a75
sushantskb <91365425+sushantskb@users.noreply.github.com>
2023-09-21 11:23:07 +0530

    succesfully connected to database and added the some dummy data

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,3 +1,29 @@
-exports.homepage = async (req, res)=>{
-    res.render("index", {title: "Cooking Page:HomePage"});
+require("../models/database");
+const Category = require("../models/Category");
+
+exports.homepage = async (req, res) => {
+  res.render("index", { title: "Cooking Page:HomePage" });
+};
+
+async function insertDummyCategoryData() {
+  try {
+    await Category.insertMany([
+      {
+        name: "Thai",
+        image: "thai-food.jpg",
+      },
+      {
+        name: "American",
+        image: "american-food.jpg",
+      },
+      {
+        name: "Chinese",
+        image: "chinese-food.jpg",
+      },
+    ]);
+  } catch (error) {
+    console.log("error: " + error);
+  }
 }
+
+insertDummyCategoryData();
---
+++ server/models/Category.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required : "This is required"
+    },
+    image: {
+        type:String ,
+        require: "This is required"
+    }
+});
+
+module.exports = mongoose.model("Category", categorySchema);
