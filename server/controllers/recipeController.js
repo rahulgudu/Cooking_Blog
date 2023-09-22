a201d7a6e1ce0b2a371c009e40fe9c1ac9fc8952
sushantskb <91365425+sushantskb@users.noreply.github.com>
2023-09-22 10:45:27 +0530

    dynamically displayed the data from database

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -2,28 +2,48 @@ require("../models/database");
 const Category = require("../models/Category");
 
 exports.homepage = async (req, res) => {
-  res.render("index", { title: "Cooking Page:HomePage" });
+  try{ 
+    const limitNumber = 5;
+    const categories = await Category.find({}).limit(limitNumber);
+    res.render("index", { title: "Cooking Page:HomePage", categories });
+  } catch(error){
+    res.status(500).send({message: error.message || "Error Occcured"});
+  }
 };
 
-async function insertDummyCategoryData() {
-  try {
-    await Category.insertMany([
-      {
-        name: "Thai",
-        image: "thai-food.jpg",
-      },
-      {
-        name: "American",
-        image: "american-food.jpg",
-      },
-      {
-        name: "Chinese",
-        image: "chinese-food.jpg",
-      },
-    ]);
-  } catch (error) {
-    console.log("error: " + error);
-  }
-}
 
-insertDummyCategoryData();
+// --I have commented the below codes, as it will keep on adding the same data to the database--
+// async function insertDummyCategoryData() {
+//   try {
+//     await Category.insertMany([
+//       {
+//         name: "Thai",
+//         image: "thai-food.jpg",
+//       },
+//       {
+//         name: "American",
+//         image: "american-food.jpg",
+//       },
+//       {
+//         name: "Chinese",
+//         image: "chinese-food.jpg",
+//       },
+//       {
+//         name: "Mexican",
+//         image: "mexican-food.jpg"
+//       },
+//       {
+//         name: "Indian",
+//         image: "indian-food.jpg"
+//       },
+//       {
+//         name: "Spanish",
+//         image: "spanish-food.jpg"
+//       },
+//     ]);
+//   } catch (error) {
+//     console.log("error: " + error);
+//   }
+// }
+
+// insertDummyCategoryData();
