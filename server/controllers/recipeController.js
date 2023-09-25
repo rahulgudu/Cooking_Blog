9047995731021ebdb85e83a2e54e5b251d453705
sushantskb <91365425+sushantskb@users.noreply.github.com>
2023-09-25 11:27:01 +0530

    added the logic to explore-latest and explore-random buttons

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -80,12 +80,35 @@ exports.searchRecipe = async (req, res) =>{
   } catch (error) {
     res.status(500).send({ message: error.message || "Error Occured" });
   }
+}
+// --end--
 
-  res.render("search", {title: "Cooking Blog - Search"});
+// --get /explore-latest--
+exports.exploreLatest = async (req, res) => {
+  try {
+    let limitNumber = 20;
+    const recipe = await Recipe.find({}).sort({_id : -1}).limit(limitNumber);
+    res.render("explore-latest", { title: "Cooking Blog - Latest Recipies", recipe });
+  } catch (error) {
+    res.status(500).send({ message: error.message || "Error Occured" });
+  }
 }
 // --end--
 
 
+// -- get/explore-random--
+exports.exploreRandom = async (req, res) =>{
+  try {
+    let count = await Recipe.find().countDocuments();
+    let random = Math.floor(Math.random() * count);
+    let recipe = await Recipe.findOne().skip(random).exec();
+    // res.json(recipe);
+    res.render("explore-random", { title: "Cooking Blog - Explore Random", recipe });
+  } catch (error) {
+    res.status(500).send({message: error.message || "Error Occurred"});
+  }
+}
+// --end--
 
 
 
